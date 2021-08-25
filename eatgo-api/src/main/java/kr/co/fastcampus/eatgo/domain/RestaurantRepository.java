87f24002dd9e61ba921fecccf3b790aea598a2bd
wonho choi <30523167+choiwonho@users.noreply.github.com>
2021-08-25 20:54:36 +0900

    Study for SpyBean annotation and add MenuItem domain (#2)
    Study about DI and add MenuItem Layer

--- eatgo-api/src/main/java/kr/co/fastcampus/eatgo/domain/RestaurantRepository.java
+++ eatgo-api/src/main/java/kr/co/fastcampus/eatgo/domain/RestaurantRepository.java
@@ -1,26 +1,9 @@
 package kr.co.fastcampus.eatgo.domain;
 
-import java.util.ArrayList;
 import java.util.List;
 
-public class RestaurantRepository {
-    private List<Restaurant> restaurants = new ArrayList<>();
+public interface RestaurantRepository {
+    List<Restaurant> findAll();
 
-    public RestaurantRepository() {
-        restaurants.add(new Restaurant(1004L, "Wonos", "Daegu"));
-        restaurants.add(new Restaurant(2020L, "heecul", "Seoul"));
-    }
-
-    public List<Restaurant> findAll() {
-
-        return restaurants;
-    }
-
-    public Restaurant findById(Long id) {
-       return restaurants.stream()
-            .filter(r -> r.getId().equals(id))
-            .findFirst()
-            .orElse(null);
-
-    }
+    Restaurant findById(Long id);
 }
