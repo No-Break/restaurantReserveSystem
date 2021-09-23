e82b3a76c7865eda743e1ba38147cf8f010b6ca7
choiwonho <jsa1031109@naver.com>
2021-09-24 07:03:54 +0900

    Test

--- eatgo-api/src/main/java/kr/co/fastcampus/eatgo/domain/Review.java
+++ eatgo-api/src/main/java/kr/co/fastcampus/eatgo/domain/Review.java
@@ -2,11 +2,14 @@ package kr.co.fastcampus.eatgo.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 
 @Entity
 @Builder
@@ -16,7 +19,15 @@ public class Review {
 
     @Id
     @GeneratedValue
+    @Getter
     private Long id;
+
+    @NotEmpty
+    private String name;
+    @NotNull
+    private Integer score;
+    @NotEmpty
+    private String description;
 
     public void setId(Long id) {
         this.id = id;
---
+++ eatgo-api/src/test/java/kr/co/fastcampus/eatgo/application/ReviewServiceTest.java
@@ -0,0 +1,38 @@
+package kr.co.fastcampus.eatgo.application;
+
+import kr.co.fastcampus.eatgo.domain.Review;
+import kr.co.fastcampus.eatgo.domain.ReviewRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+
+class ReviewServiceTest {
+
+    private ReviewService reviewService;
+
+    @Mock
+    private ReviewRepository reviewRepository;
+
+    @BeforeEach
+    public void setUp() {
+        reviewService = new ReviewService();
+    }
+
+    @Test
+    public void addReview() {
+        Review review = Review.builder()
+                .name("Joker")
+                .score(3)
+                .description("Good")
+                .build() ;
+        reviewService.addReview(review);
+
+        verify(reviewRepository).save(any());
+    }
+
+}
